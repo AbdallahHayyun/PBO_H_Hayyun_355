fb227654cebb391a1a59eee4b702af9f5d72486e
AbdallahHayyun <hayyun06@gmail.com>
2025-05-04 22:03:02 +0700

    Add files via upload

---
+++ TUGAS/PBOPRAKTIKUM/src/Mahasiswa.java
@@ -0,0 +1,16 @@
+public class Mahasiswa extends User {
+    public Mahasiswa(String nama, String nim) {
+        super(nama, nim);
+    }
+
+    @Override
+    public boolean login(String inputNama, String inputNim) {
+        return inputNama.equals(getNama()) && inputNim.equals(getNim());
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Login Mahasiswa Berhasil!");
+        super.displayInfo(); // Memanggil method superclass
+    }
+}
